9da4c63900dd5719f35ad5947ee27a60d8fb64af
EMillison <emillison@mymail.mines.edu>
2019-11-15 18:19:28 -0700

    created LoadFile function

--- Week12-utility.py
+++ Week12-utility.py
@@ -6,3 +6,15 @@
 #PrintOutput:
 def PrintOutput(input_str):
     print("OUTPUT", input_str)
+
+#LoadFile:
+def LoadFile(filename):
+    file = open(filename, "r+")
+    file_list = file.readlines()
+
+    new_list = []
+    for i in range(len(file_list)):
+        stringi = file_list[i].replace('\n', '')
+        new_list.append(stringi)
+    return new_list
+
