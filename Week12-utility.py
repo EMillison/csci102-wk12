e22072021c3d7842a83466bddb3113527aae2be3
EMillison <emillison@mymail.mines.edu>
2019-11-15 17:50:58 -0700

    created a print function

---
+++ Week12-utility.py
@@ -0,0 +1,8 @@
+# INSERT GITHUB REPOSITORY HEREEEEEEEEEEEEEEEEEEEEE
+# Emily Millison
+# CSCI 102 - Section A
+# Week 12 - Part A
+
+#PrintOutput:
+def PrintOutput(input_str):
+    print("OUTPUT", input_str)
