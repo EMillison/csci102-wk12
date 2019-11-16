ebbdc4a15af90e484b8fddace6d9bdc91674d73a
EMillison <emillison@mymail.mines.edu>
2019-11-15 18:31:04 -0700

    created UpdateString function

--- Week12-utility.py
+++ Week12-utility.py
@@ -18,3 +18,14 @@ def LoadFile(filename):
         new_list.append(stringi)
     return new_list
 
+#UpdateString:
+def UpdateString(string1, string2, index):
+    string1_list = list(string1) #list function from here https://stackoverflow.com/questions/4978787/how-to-split-a-string-into-array-of-characters/29225966
+    print(string1_list)
+    string1_list[index] = string2
+
+    string3 = ''
+    for i in range(len(string1_list)):
+        string3 += string1_list[i]
+    return string3
+    
