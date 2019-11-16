592b5e517d44333ae32bc0e52764bf5f439c0926
EMillison <emillison@mymail.mines.edu>
2019-11-16 14:44:28 -0700

    Created ScoreFinder function

--- Week12-utility.py
+++ Week12-utility.py
@@ -8,6 +8,7 @@ def PrintOutput(input_str):
     print("OUTPUT", input_str)
 
 #LoadFile:
+#Return a list that is the length of the number of lines of the file and where each element is a string that is the text in that line
 def LoadFile(filename):
     file = open(filename, "r+")
     file_list = file.readlines()
@@ -19,17 +20,18 @@ def LoadFile(filename):
     return new_list
 
 #UpdateString:
+#Take in two strings and an index. Prints a string where the first stringn is modified by replacing the character at the index with the second string
 def UpdateString(string1, string2, index):
     string1_list = list(string1) #list function from here https://stackoverflow.com/questions/4978787/how-to-split-a-string-into-array-of-characters/29225966
-    print(string1_list)
     string1_list[index] = string2
 
     string3 = ''
     for i in range(len(string1_list)):
         string3 += string1_list[i]
-    return string3
+    print("OUTPUT", string3)
 
 #FindWordCount:
+#find the amount of times that string1 appears in list 1
 def FindWordCount(list1, string1):
     counter = 0
     for word in list1:
@@ -38,4 +40,20 @@ def FindWordCount(list1, string1):
         else:
             pass
     return counter
+
+#ScoreFinder:
+#Given a list of names, scores, and a certain player name, find the player name in the list, and find their corresponding score in the second list (it will be at the same index)
+def ScoreFinder(player_names_list, player_scores_list, player_name):
+    player_names_list2 = []
+    for i in range(len(player_names_list)):
+        new_name = player_names_list[i].lower()
+        player_names_list2.append(new_name)
+    if player_name.lower() in player_names_list2:
+        player_index = player_names_list2.index(player_name.lower())
+        player_score = player_scores_list[player_index]
+        output_str = 'OUTPUT ' + player_names_list[player_index] + ' got a score of ' + str(player_score)
+        print(output_str)
+    else:
+        error_string = "OUTPUT player not found"
+        print(error_string)
     
